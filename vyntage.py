01e7bf47203c0c4d8d7e1e29ff812d79ee9a9123
Marc Laugharn <mlaugharn@gmail.com>
2011-09-11 11:15:15 -0700

    made it possible to use multiple filetypes

--- vyntage.py
+++ vyntage.py
@@ -1,19 +1,18 @@
 import Image, ImageChops, ImageEnhance, random, sys
 
 def distort(image_file):
-        try:
-                x = Image.open(image_file)
-                x.load()
-                red, green, blue = x.split()
-                green = ImageChops.offset(green, random.randint(0,3),
-                    random.randint(0,3))
-                blue = ImageChops.offset(ImageEnhance.Contrast(blue).enhance(.4),
-                    random.randint(0,5),
-                    random.randint(0,5))
-                Image.merge("RGB", (red, green, blue)).save("vyntage_%s.jpg" %
-                                                            image_file)
-        except IOError:
-                print "Incompatible file type"
+        x = Image.open(image_file, "r")
+        x.load()
+        red, green, blue = x.split()
+        green = ImageChops.offset(green, random.randint(0,3),
+            random.randint(0,3))
+        blue = ImageChops.offset(ImageEnhance.Contrast(blue).enhance(.4),
+            random.randint(0,5),
+            random.randint(0,5))
+        Image.merge("RGB", (red, green, blue)).save("vyntage_%s" %
+                                                    image_file)
+##        except IOError:
+##                print "Incompatible file type"
         
 
 if __name__ == "__main__":
